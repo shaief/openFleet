22f6e8e75c18e83c6ee78e1c6c12dab1ba016212
shaief <shaief@gmail.com>
2014-03-04 20:28:06 +0200

    home now present notifications

--- effectiveCar/views/views_general.py
+++ effectiveCar/views/views_general.py
@@ -1,8 +1,55 @@
 from django.shortcuts import render
+
+from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from effectiveCar.models import Car, Owner, Classification
+import datetime
+import json
+import time
+import itertools
+
+from django.db.models import Avg
+
 # Create your views here.
 
+# def home(request):
+    # return render(request, 'effectiveCar/index.html')
+
+
 def home(request):
-    return render(request, 'effectiveCar/index.html')
-
-
-
+    today = datetime.datetime.now()
+    next_week = datetime.datetime.now()+datetime.timedelta(days=7)
+    next_month = datetime.datetime.now()+datetime.timedelta(days=31)
+    next_year = datetime.datetime.now()+datetime.timedelta(days=365)
+    license_next_week = Car.objects.all().\
+        filter(license_renewal_date__lte=next_week).\
+        order_by('license_renewal_date')
+    license_next_month = Car.objects.all().\
+        filter(license_renewal_date__lte=next_month).\
+        filter(license_renewal_date__gte=next_week).\
+        order_by('license_renewal_date')
+    license_next_year = Car.objects.all().\
+        filter(license_renewal_date__lte=next_year).\
+        filter(license_renewal_date__gte=next_month).\
+        order_by('license_renewal_date')
+    insurance_next_week = Car.objects.all().\
+        filter(insurance_renewal_date__lte=next_week).\
+        order_by('license_renewal_date')
+    insurance_next_month = Car.objects.all().\
+        filter(insurance_renewal_date__lte=next_month).\
+        filter(insurance_renewal_date__gte=next_week).\
+        order_by('license_renewal_date')
+    insurance_next_year = Car.objects.all().\
+        filter(insurance_renewal_date__lte=next_year).\
+        filter(insurance_renewal_date__gte=next_month).\
+        order_by('license_renewal_date')
+    context = dict(
+        today=today,
+        license_next_week=license_next_week,
+        license_next_month=license_next_month,
+        license_next_year=license_next_year,
+        insurance_next_week=insurance_next_week,
+        insurance_next_month=insurance_next_month,
+        insurance_next_year=insurance_next_year,
+    )
+    return render(request, 'effectiveCar/index.html', context)
