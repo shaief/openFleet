b05bd4249ec8124187f966e2edb9ce64822a8250
shaief <shaief@gmail.com>
2014-03-11 12:27:27 +0200

    AWS S3 for static files

--- openFleet/settings/heroku.py
+++ openFleet/settings/heroku.py
@@ -13,3 +13,17 @@ DATABASES['default'] = dj_database_url.config()
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
 
 ALLOWED_HOSTS = ['openfleet.herokuapp.com']
+
+# copied from openAir dokku.py:
+STATICFILES_STORAGE = DEFAULT_FILE_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
+AWS_ACCESS_KEY_ID = environ.get('S3_KEY')
+AWS_SECRET_ACCESS_KEY = environ.get('S3_SECRET')
+AWS_STORAGE_BUCKET_NAME = environ.get('AWS_STORAGE_BUCKET_NAME')
+AWS_QUERYSTRING_AUTH = False
+AWS_PRELOAD_METADATA = True
+AWS_IS_GZIPPED = True
+AWS_S3_SECURE_URLS = False
+AWS_HEADERS = {
+    # 'Cache-Control': 'public, max-age=86400',
+}
+STATIC_URL = 'https://{}.s3.amazonaws.com/'.format(AWS_STORAGE_BUCKET_NAME)
