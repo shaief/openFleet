1ac45d227ca42a6250831f9ce4c54cc33b27c5db
shaief <shaief@gmail.com>
2014-03-26 19:39:21 +0200

    added django-braces. refernced SECRET_KEY to environ

--- openFleet/settings/base.py
+++ openFleet/settings/base.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = (
     'south',
     # 'debug_toolbar',
     'dj_database_url',
+    'braces',
 
     'effectiveCar',
 )
--- openFleet/settings/heroku.py
+++ openFleet/settings/heroku.py
@@ -20,6 +20,8 @@ DEFAULT_FILE_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
 ALLOWED_HOSTS = ['openfleet.herokuapp.com']
 
 # copied from openAir dokku.py:
+SECRET_KEY = environ.get('SECRET_KEY')
+
 STATICFILES_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
 DEFAULT_FILE_STORAGE = STATICFILES_STORAGE
 AWS_ACCESS_KEY_ID = environ.get('S3_KEY')
